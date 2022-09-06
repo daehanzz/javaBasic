0018dd05f51c6c1aaa89328bd6bb6f3b9d5a289d
daehanzz <qdpq02@naver.com>
2022-09-06 17:45:23 +0900

    변수 1번예제

--- src/variable/VarEx1.java
+++ src/variable/VarEx1.java
@@ -4,7 +4,18 @@ public class VarEx1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		//ddddd
+		
+		int year=0;
+		int age=14;
+		
+		System.out.println(year);	//0
+		System.out.println(age);	//14
+		
+		year=age+2000; // 변수 age의 값에 2000을 더해서 변수 year에 저장
+		age=age+1;	   // 변수 age에 저장된 값을 1 증가시킨다.
+		
+		System.out.println(year);	//2014
+		System.out.println(age);	//15
 	}
 
 }
