28bc154959bda4e892132bb06a15c3aa679ebfcc
daehanzz <qdpq02@naver.com>
2022-09-08 13:22:37 +0900

    자료형 형변환

---
+++ src/variable/CharToCode.java
@@ -0,0 +1,18 @@
+package variable;
+
+public class CharToCode {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//문자형 형변환
+		char ch = 'A';	//char ch = 65;
+		int code = (int)ch;	//ch에 저장된 값을 int타입으로 변환하여 저장
+		
+		System.out.printf("%c=%d(%#X)%n",ch,code,code);
+		
+		char hch = '가';	//char hch = 0xAC00;
+		System.out.printf("%c=%d(%#X)%n",hch,(int)hch,(int)hch);
+	}
+
+}
