3a28ce8c3c616a265fc6e7dc9af5bbdd1aa2d7cd
daehanzz <qdpq02@naver.com>
2022-09-11 17:24:12 +0900

    형변환 1,2,3 예제

---
+++ src/variable/CastingEx1.java
@@ -0,0 +1,15 @@
+package variable;
+
+public class CastingEx1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		double d = 85.4;
+		int score = (int)d;
+		
+		System.out.println("score = "+score);
+		System.out.println("d = "+d); //85.4 형변환 후에도 피연한자는 아무 변화가 없다
+	}
+
+}
