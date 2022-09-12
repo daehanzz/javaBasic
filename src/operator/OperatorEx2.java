59eade5e4dba4812129eb97d08badbd25ad15ab7
daehanzz <qdpq02@naver.com>
2022-09-12 12:00:28 +0900

    operator 2번 예제

---
+++ src/operator/OperatorEx2.java
@@ -0,0 +1,21 @@
+package operator;
+
+public class OperatorEx2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int i = 5, j = 0;
+		
+		j = i++;
+		System.out.println("j = i++; 실행후, i = "+i+", j = "+j);
+		
+		i = 5;	// 결과를 비교하기 위해, i와 j의 값을 다시 5와 0으로 변경
+		j = 0;
+		
+		j = ++i;
+		System.out.println("j = ++i; 실행후, i = "+i+", j = "+j);
+		
+	}
+
+}
