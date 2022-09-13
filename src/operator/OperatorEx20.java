7c0266efae93411569ccececfdeec8f1a87d410f
daehanzz <qdpq02@naver.com>
2022-09-13 16:43:32 +0900

    Operator 19,20,21,22,23 예제

---
+++ src/operator/OperatorEx20.java
@@ -0,0 +1,18 @@
+package operator;
+
+public class OperatorEx20 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println(-10%8);
+		System.out.println(10%-8);
+		System.out.println(-10%-8);
+		
+		/*
+		 * 나머지 연산자는 음수도 허용하지만
+		 * 부호는 무시되므로 결과는 음수의 절대값으로 나눈 나머지와 결과가 같다.
+		 * */
+	}
+
+}
