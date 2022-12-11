f65a5efa8d946d8d1e72cbba621b6f6e60e3ed68
daehanzz <qdpq02@naver.com>
2022-12-11 22:22:16 +0900

    컬렉션 프레임웍 예제들

---
+++ src/ch11/TreeSetEx2.java
@@ -0,0 +1,23 @@
+package ch11;
+
+import java.util.TreeSet;
+
+public class TreeSetEx2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		TreeSet set = new TreeSet<>();
+		
+		int[] score = {80,95,50,35,45,65,10,100};
+		
+		for(int i=0; i<score.length; i++) {
+			set.add(score[i]);
+		}
+		
+		System.out.println(set);
+		System.out.println("50보다 작은 값 : "+set.headSet(50));
+		System.out.println("50보다 큰 값 : "+set.tailSet(50));
+	}
+
+}
